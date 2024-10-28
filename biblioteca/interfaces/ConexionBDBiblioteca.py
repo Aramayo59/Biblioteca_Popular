9dda5b4d3fa1d920875f653ba4ceb12e9389dfe0
Nivrox <nicopro.git@gmail.com>
2024-10-28 01:20:44 -0300

    Ultimos cambios Bruno

---
+++ biblioteca/interfaces/ConexionBDBiblioteca.py
@@ -0,0 +1,16 @@
+import mysql.connector
+class Cconexion:
+    
+ def conexion():
+       
+    try:
+            conexion = mysql.connector.connect(user="root",
+            host="localhost",                            
+            password="bru455no", 
+            database="BibliotecaJoseHporto")  
+            print ("Conexion Correcta")
+            return conexion           
+    except mysql.connector.Error as error: 
+        print ("Error AL conectarte a la base de datos{}".format(error))
+            
+        return conexion
